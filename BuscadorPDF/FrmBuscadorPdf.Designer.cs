0175d28074c4103e8fa61591bdcf99b11e1b3e44
Fernando <frbatist@gmail.com>
2016-06-02 13:30:52 -0300

    Melhorias de usabilidade, métodos assincronos.

---
+++ BuscadorPDF/FrmBuscadorPdf.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace BuscadorPDF
+{
+    partial class FrmBuscadorPdf
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmBuscadorPdf));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Arquivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Caminho = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TxtPDF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.groupArquivos = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnMostraEscondeArquivos = new System.Windows.Forms.Button();
+            this.btnFonteArquivo = new System.Windows.Forms.Button();
+            this.seletorPasta = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.groupArquivos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1238, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Busca";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1219, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Arquivo,
+            this.Caminho,
+            this.TxtPDF});
+            this.dataGridView1.Location = new System.Drawing.Point(47, 337);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(170, 176);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            // 
+            // Arquivo
+            // 
+            this.Arquivo.DataPropertyName = "Arquivo";
+            this.Arquivo.HeaderText = "Arquivo";
+            this.Arquivo.Name = "Arquivo";
+            this.Arquivo.ReadOnly = true;
+            this.Arquivo.Width = 250;
+            // 
+            // Caminho
+            // 
+            this.Caminho.DataPropertyName = "Caminho";
+            this.Caminho.HeaderText = "Caminho";
+            this.Caminho.Name = "Caminho";
+            this.Caminho.ReadOnly = true;
+            this.Caminho.Visible = false;
+            // 
+            // TxtPDF
+            // 
+            this.TxtPDF.DataPropertyName = "TxtPDF";
+            this.TxtPDF.HeaderText = "TxtPDF";
+            this.TxtPDF.Name = "TxtPDF";
+            this.TxtPDF.ReadOnly = true;
+            this.TxtPDF.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1211, 623);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Abrir PDF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.axAcroPDF1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(977, 545);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PDF";
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(6, 19);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(971, 520);
+            this.axAcroPDF1.TabIndex = 0;
+            // 
+            // groupArquivos
+            // 
+            this.groupArquivos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupArquivos.Controls.Add(this.pictureBox1);
+            this.groupArquivos.Controls.Add(this.dataGridView1);
+            this.groupArquivos.Location = new System.Drawing.Point(3, 3);
+            this.groupArquivos.Name = "groupArquivos";
+            this.groupArquivos.Size = new System.Drawing.Size(307, 545);
+            this.groupArquivos.TabIndex = 6;
+            this.groupArquivos.TabStop = false;
+            this.groupArquivos.Text = "Arquivos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BuscadorPDF.Properties.Resources.gifCarregando;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 121);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(13, 66);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupArquivos);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1300, 551);
+            this.splitContainer1.SplitterDistance = 313;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // btnMostraEscondeArquivos
+            // 
+            this.btnMostraEscondeArquivos.Location = new System.Drawing.Point(13, 39);
+            this.btnMostraEscondeArquivos.Name = "btnMostraEscondeArquivos";
+            this.btnMostraEscondeArquivos.Size = new System.Drawing.Size(143, 23);
+            this.btnMostraEscondeArquivos.TabIndex = 8;
+            this.btnMostraEscondeArquivos.Text = "Esconde arquivos";
+            this.btnMostraEscondeArquivos.UseVisualStyleBackColor = true;
+            this.btnMostraEscondeArquivos.Click += new System.EventHandler(this.btnMostraEscondeArquivos_Click);
+            // 
+            // btnFonteArquivo
+            // 
+            this.btnFonteArquivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFonteArquivo.Location = new System.Drawing.Point(13, 623);
+            this.btnFonteArquivo.Name = "btnFonteArquivo";
+            this.btnFonteArquivo.Size = new System.Drawing.Size(1192, 23);
+            this.btnFonteArquivo.TabIndex = 9;
+            this.btnFonteArquivo.Text = "Fonte arquivo: ";
+            this.btnFonteArquivo.UseVisualStyleBackColor = true;
+            this.btnFonteArquivo.Click += new System.EventHandler(this.btnFonteArquivo_Click);
+            // 
+            // FrmBuscadorPdf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 658);
+            this.Controls.Add(this.btnFonteArquivo);
+            this.Controls.Add(this.btnMostraEscondeArquivos);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmBuscadorPdf";
+            this.Text = "BuscadorPDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.groupArquivos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Arquivo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Caminho;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtPDF;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.GroupBox groupArquivos;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnMostraEscondeArquivos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnFonteArquivo;
+        private System.Windows.Forms.FolderBrowserDialog seletorPasta;
+    }
+}
+
---
+++ BuscadorPDF/FrmBuscadorPdf.cs
@@ -0,0 +1,193 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscadorPDF
+{
+    public partial class FrmBuscadorPdf : Form
+    {
+        public List<PDFCarregado> listaArquivos { get; set; }
+        private bool _listagemArquivosVisivel;
+        private string _fonteArquivos;
+        public FrmBuscadorPdf()
+        {
+            InitializeComponent();
+            dataGridView1.AutoGenerateColumns = false;
+            _listagemArquivosVisivel = true;
+            dataGridView1.Visible = false;
+            _fonteArquivos = @"C:\Zuera\";
+            btnFonteArquivo.Text = "Fonte arquivos: " + _fonteArquivos;
+        }
+
+        public async Task CarregaDadosArquivos()
+        {
+            pictureBox1.Visible = true;
+            dataGridView1.Visible = false;
+
+            listaArquivos = new List<PDFCarregado>();
+            var dir = new DirectoryInfo(_fonteArquivos);
+
+            string msgErro = "";
+
+            FileInfo[] files = await Task.Factory.StartNew(() =>
+            {
+                FileInfo[] arquivos = null;
+                try
+                {
+                    arquivos = dir.GetFiles("*.pdf", SearchOption.AllDirectories);
+                }
+                catch (Exception ex)
+                {
+                    msgErro = ex.Message;                    
+                }                
+                return arquivos;
+            });
+            if (!string.IsNullOrEmpty(msgErro))
+            {
+                pictureBox1.Visible = false;
+                throw new Exception(msgErro);
+            }
+            for (int i = 0; i <= files.Length - 1; i++)
+            {
+                listaArquivos.Add(await ObterObjetoPDf(files[i]));
+            }
+            CarregaGrid(textBox1.Text);
+            pictureBox1.Visible = false;
+            dataGridView1.Visible = true;
+            dataGridView1.Dock = DockStyle.Fill;
+
+        }
+
+        private async Task<PDFCarregado> ObterObjetoPDf(FileInfo fileInfo)
+        {
+            string txtPdf = await ObterTextoPdf(fileInfo.FullName);            
+            return new PDFCarregado { Arquivo = fileInfo.Name, Caminho = fileInfo.FullName, TxtPDF = txtPdf };
+        }
+
+        private async Task<string> ObterTextoPdf(string fullName)
+        {
+            using (PdfReader reader = new PdfReader(fullName))
+            {
+                StringBuilder text = new StringBuilder();
+                for (int j = 1; j <= reader.NumberOfPages; j++)
+                {
+                    text.Append(await Task.Factory.StartNew(() =>
+                    {
+                        return PdfTextExtractor.GetTextFromPage(reader, j);
+                    }));
+                }
+                return text.ToString().ToUpper();
+            }
+        }
+
+        private async void CarregaGrid(string filtro)
+        {
+            List<PDFCarregado> sourceConsulta = await ObterFonteDados();
+            if (!string.IsNullOrEmpty(filtro))            
+            {
+                var palavras = filtro.Split(' ');
+                foreach (var item in palavras)
+                {                   
+                    sourceConsulta = await FiltraArquivosPorTermo(sourceConsulta, item);
+                }
+            }
+            dataGridView1.DataSource = sourceConsulta;
+        }
+
+        private Task<List<PDFCarregado>> FiltraArquivosPorTermo(List<PDFCarregado> sourceConsulta, string termo)
+        {
+            return Task.Run(() =>
+            {
+                return sourceConsulta.Where(d => d.TxtPDF.Contains(termo.Trim().ToUpper())).ToList();
+            });
+        }
+
+        private Task<List<PDFCarregado>> ObterFonteDados()
+        {
+            return Task.Run(() =>
+            {
+                return (from a in listaArquivos
+                        select a).ToList();
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CarregaGrid(textBox1.Text);
+        }
+
+        public class PDFCarregado
+        {
+            public string Arquivo { get; set; }
+            public string Caminho { get; set; }
+            public string TxtPDF { get; set; }
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex] != null)
+            {
+                var caminho = dataGridView1.Rows[e.RowIndex].Cells["Caminho"].Value.ToString();
+                var acro = (AcroPDFLib.IAcroAXDocShim)axAcroPDF1.GetOcx();
+                acro.LoadFile(caminho);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var caminho = dataGridView1.CurrentRow.Cells["Caminho"].Value.ToString();
+                Process.Start(caminho);
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                await CarregaDadosArquivos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnMostraEscondeArquivos_Click(object sender, EventArgs e)
+        {
+            if (_listagemArquivosVisivel)
+            {
+                splitContainer1.Panel1Collapsed = true;
+                splitContainer1.Panel1.Hide();
+                _listagemArquivosVisivel = false;
+                btnMostraEscondeArquivos.Text = "Mostra arquivos";
+            }
+            else
+            {
+                splitContainer1.Panel1Collapsed = false;
+                splitContainer1.Panel1.Show();
+                _listagemArquivosVisivel = true;
+                btnMostraEscondeArquivos.Text = "Esconde arquivos";
+            }
+        }
+
+        private async void btnFonteArquivo_Click(object sender, EventArgs e)
+        {
+            if (seletorPasta.ShowDialog() == DialogResult.OK)
+            {
+                _fonteArquivos = seletorPasta.SelectedPath;
+                btnFonteArquivo.Text = "Fonte arquivos: " + _fonteArquivos;
+                await CarregaDadosArquivos();
+            }
+        }
+    }
+}
